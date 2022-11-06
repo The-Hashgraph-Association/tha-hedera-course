47a8b1f221979fe8ebd3fd0be854d478f63cbdef
Sparkes Ian Thomas, B2B-BAN-PDC-T-IIF <IanThomas.Sparkes@swisscom.com>
2022-11-06 11:18:51 +0100

    Simplify to use a fixed Account ID

--- Account/scripts/getOtherAccountInfo.js
+++ Account/scripts/getOtherAccountInfo.js
@@ -5,13 +5,11 @@ const {
 
 // const myAccountId = "0.0.47664706";
 // const myPrivateKey = "302e020100300506032b657004220420c1e58e47370a0135097e843a9d56b3531b59d9265b30c12b185abcaa7570181a";
-// 
-// const otherAccountId = "0.0.48813362";
 
 const myAccountId = "put your account ID here";
 const myPrivateKey = "put your private key here";
 
-const otherAccountId = "put the other account ID here";
+const otherAccountId = "0.0.48813362";
 
 async function main() {
     // Create our connection to the Hedera network
