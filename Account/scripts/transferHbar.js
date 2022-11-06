0aa35af54f6c8af3ab203ade65ed8c4862f818c0
Sparkes Ian Thomas, B2B-BAN-PDC-T-IIF <IanThomas.Sparkes@swisscom.com>
2022-11-06 11:59:24 +0100

    Add transfer script

---
+++ Account/scripts/transferHbar.js
@@ -0,0 +1,56 @@
+const {
+    Client,
+    AccountBalanceQuery,
+    TransferTransaction,
+    Hbar
+} = require("@hashgraph/sdk");
+
+// const myAccountId = "0.0.48813362";
+// const myPrivateKey = "9dcc33fcdea6bbe71ccd45012743518767c144c057824f8228f6abfc5be1fb4e";
+
+const myAccountId = "put your account ID here";
+const myPrivateKey = "put your private key here";
+
+const otherAccountId = "0.0.47664706";
+
+async function main() {
+    // Create our connection to the Hedera network
+    // The Hedera JS SDK makes this really easy!
+    const client = Client.forTestnet();
+
+    client.setOperator(myAccountId, myPrivateKey);
+
+    //Create the transfer transaction
+    const transaction = new TransferTransaction()
+    .addHbarTransfer(myAccountId, new Hbar(-10))
+    .addHbarTransfer(otherAccountId, new Hbar(10));
+    
+    console.log(`Doing transfer from ${myAccountId} to ${otherAccountId}`);
+
+    //Sign with the client operator key and submit the transaction to a Hedera network
+    const txId = await transaction.execute(client);
+
+    //Request the receipt of the transaction
+    const receipt = await txId.getReceipt(client);
+
+    //Get the transaction consensus status
+    const transactionStatus = receipt.status;
+
+    console.log("The transaction consensus status is " +transactionStatus);
+
+    //Create the queries
+    const queryMine = new AccountBalanceQuery()
+     .setAccountId(myAccountId);
+
+    //Create the queries
+    const queryOther = new AccountBalanceQuery()
+     .setAccountId(otherAccountId);
+
+    const accountBalanceMine = await queryMine.execute(client);
+
+    const accountBalanceOther = await queryOther.execute(client);
+
+    console.log(`My account balance ${accountBalanceMine.hbars} HBar, other account balance ${accountBalanceOther.hbars}`);
+}
+
+main();
